42e60a01b93a1241a60f5c7147baa15a1a9d1999
TsionAH <tsionalemuhandiso@gmail.com>
2025-07-16 05:49:51 -0700

    mu

---
+++ hello.js
@@ -0,0 +1,27 @@
+/*let age = 25;
+console.log(age);
+age = 30;
+console.log(age);
+let fname = 'tsion';
+let lname = 'alemu';
+let fullname = fname + " " + lname;
+let slice = fullname.slice(0,4);
+let resulu = `the blog called ${fname} and her father name is ${lname}`;
+console.log(resulu);
+let html = `
+<h1> ${fname} </h1> 
+`;
+console.log(html);
+let arr = ['abee' , 'ayele' , 'mark'];
+let re = arr.join('-');
+console.log(re);
+let check = fname.includes('j');
+// in js we use === to restrict comparion but == we can compare and kind of data type
+// to change string of numbet in to int we usse
+let score = '22'
+score = Number(score);
+console.log(score);*/
+for(let i = 0; i < 5 ; i++){
+    console.log('in loop:' , i);
+}
+const names = ['shah' , 'tsion ', ' alemu'];
